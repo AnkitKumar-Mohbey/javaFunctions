2aa7fee2831917964d2e86f31ad92cf4740d3ca0
I ANKIT KUMAR <91773999+THELOVERCHOICE@users.noreply.github.com>
2023-06-20 08:19:19 +0530

    Add files via upload

---
+++ sumOfNum.java
@@ -0,0 +1,18 @@
+package theLoverChoice;
+
+public class sumOfNum {
+	
+	public static int sum(int num1, int num2) {
+		int ans = num1 + num2;
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		int ans = sum(num1,num2);
+	    System.out.println(ans);
+	}
+
+}
