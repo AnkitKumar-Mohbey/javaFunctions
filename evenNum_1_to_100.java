8a62017a2fec77840dea7f1687524d916d5d7d73
I ANKIT KUMAR <91773999+THELOVERCHOICE@users.noreply.github.com>
2023-06-20 08:29:12 +0530

    Add files via upload

---
+++ evenNum_1_to_100.java
@@ -0,0 +1,15 @@
+package theLoverChoice;
+
+public class evenNum_1_to_100 {
+
+	public static void evenNumbers() {
+		for(int i = 0; i <= 100; i+=2) {
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args) {
+		evenNumbers();
+
+	}
+
+}
